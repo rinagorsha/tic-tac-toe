12f5fa2c9cfc19c6e8e70a36924ba09526764cc6
rinagorsha <rinagorsha@rccraft.ru>
2017-07-10 16:34:43 +0300

    init

---
+++ utils/pug.js
@@ -0,0 +1,1 @@
+require.context('../src/pug/', false, /\.pug$/);
---
+++ utils/webpack.config.js
@@ -0,0 +1,102 @@
+'use strict'
+
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const autoprefixer = require('autoprefixer-stylus');
+
+const context = path.resolve(__dirname, '../');
+module.exports = {
+	context: context,
+
+	entry: [
+		'./src/styl/main.styl',
+		'./utils/pug.js'
+	],
+	output: {
+		path: path.resolve(context, 'dist'),
+		filename: path.join('js', 'all.js')
+	},
+
+	devtool: 'source-map',
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: [/node_modules/],
+				use: [{
+					loader: 'babel-loader',
+					options: {
+						presets: ['es2015']
+					}
+				}]
+			}, {
+				test: /\.(pug|jade)$/,
+				exclude: [/node_modules/],
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[path][name].html',
+							context: 'src/pug/',
+						}
+					}, {
+						loader: 'pug-html-loader',
+						options: {
+							pretty: true
+						}
+					}
+				]
+			}, {
+				test: /\.(styl|stylus)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: path.join('css', 'all.css'),
+						},
+					}, {
+						loader: 'stylus-loader',
+						options: {
+							compress: true,
+							use: [autoprefixer({browsers: ['> 1%', 'ie > 9', 'iOS > 6'], hideWarnings: true})],
+						},
+					},
+				],
+
+			}, {
+				test: /\.(png|jpg|gif|svg|ico|eot|ttf|woff|woff2)$/,
+				use: ExtractTextPlugin.extract({
+					use: [{
+						loader: 'file-loader',
+						options: {
+							name: '[path][name].[ext]'
+						}
+					}]
+				})
+			},
+		]
+	},
+
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new CleanPlugin(['dist/'], {
+			root: context,
+			verbose: true,
+			dry: false,
+		}),
+	],
+
+	devServer: {
+		contentBase: path.resolve(__dirname, 'dist'),
+		host: 'localhost',
+		port: 3000,
+		openPage: '',
+		compress: true,
+		open: true,
+		stats: 'errors-only'
+	}
+};
